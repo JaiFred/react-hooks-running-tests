31ba8592545fa0420fc9af66c398263f8f457076
JaiVon <frederickjaivon@yahoo.com>
2022-06-26 00:11:03 -0400

    commit tree a0ce7d0f79f2a6f25c52dbec627fc89ad8cecd7f

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,20 @@
-import React from "react";
 
 function Article() {
   return (
-    <div>
-      {/* please pass this test */}
-    </div>
+    <div>please pass this test</div>
   );
 }
-
 export default Article;
+
+// test("displays the text 'please pass this test'", () => {
+//   render(<Article />);
+
+//   const element = screen.queryByText("please pass this test");
+
+//   screen.debug(element);
+
+//   expect(element).toBeInTheDocument();
+
+  
+// });
+
